a6dc88fba58e18fc0bc0c6cb7724e2568645b597
Mateo740 <118283762+Mateo740@users.noreply.github.com>
2022-11-16 10:36:06 +0100

    Update Graphe vitesses.py

--- Graphe vitesses.py
+++ Graphe vitesses.py
@@ -4,12 +4,12 @@ from matplotlib import pyplot as plt
 
 G = nx.DiGraph()
 
-nodes = np.arange(0, 7).tolist()
+nodes = np.arange(0, 8).tolist()
 G.add_nodes_from(nodes)
 
 G.add_weighted_edges_from([(0,1,0.8), (0,2, 0),
  (1,3,0.2), (1, 4,0),
- (2, 5, 0.9), (2, 6,0)])
+ (2, 5, 0.9), (2, 6,0), (3, 7,0),(4, 7,0.2),(5, 8,0),(6, 8,0.6),(7,9,0),(8,9,0.3) ])
 
 
 
@@ -17,14 +17,14 @@ elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] > 0.5]
 esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] <= 0.5]
 
 pos = {0:(0, 10),
- 1:(10, 15), 2:(10, 7.5),
- 3:(20, 17.5), 4:(20, 12.5),
- 5:(20, 7.5), 6:(20, 2.5)}
+ 1:(10, 15), 2:(10, 5),
+ 3:(20, 20), 4:(20, 12.5),
+ 5:(20, 7.5), 6:(20, 0),7:(30,15),8:(30,5),9:(40,10)}
 
 labels = {0:25,
  1:30, 2: 22,
  3: 32, 4: 28,
- 5: 26, 6: 19}
+ 5: 26, 6: 19, 7:29,8:22,9:25}
 
 L=[]
 for i in range (0,len(labels)) :
