c68c594daa41f3ecfed87e43db750e6c87ab8388
paulinelegat40 <118973794+paulinelegat40@users.noreply.github.com>
2022-11-24 11:16:26 +0100

    Add files via upload

---
+++ Djikstra_path.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import networkx as nx
+import numpy as np
+from matplotlib import pyplot as plt
+
+G = nx.DiGraph()
+
+nodes = np.arange(0, 8).tolist()
+G.add_nodes_from(nodes)
+
+W = [(0,1,0.8), (0,2, 0),
+ (1,3,0.3), (1, 4,0),
+ (2, 5, 0.9), (2, 6,0), (3, 7,0),(4, 7,0.2),(5, 8,0),(6, 8,0.6),(7,9,0),(8,9,0.3) ]
+
+G.add_weighted_edges_from(W)
+
+
+
+elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] > 0.5]
+esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d["weight"] <= 0.5]
+
+pos = {0:(0, 10),
+ 1:(10, 15), 2:(10, 5),
+ 3:(20, 20), 4:(20, 12.5),
+ 5:(20, 7.5), 6:(20, 0),7:(30,15),8:(30,5),9:(40,10)}
+
+labels = {0:25,
+ 1:30, 2: 22,
+ 3: 32, 4: 28,
+ 5: 26, 6: 19, 7:29,8:22,9:25}
+
+L=[]
+for i in range (0,len(labels)) :
+    L+=[labels[i]]
+    i+=1
+print(L)
+
+
+# nodes
+nx.draw_networkx_nodes(G, pos, node_size=700)
+
+# edges
+nx.draw_networkx_edges(G, pos, edgelist=elarge, width=6, edge_color="r", style="dashed")
+nx.draw_networkx_edges(
+    G, pos, edgelist=esmall, width=6)
+
+# # node labels
+nx.draw_networkx_labels(G, pos, labels,  font_size=6, font_family="sans-serif")
+# edge weight labels
+edge_labels = nx.get_edge_attributes(G, "weight")
+nx.draw_networkx_edge_labels(G, pos, edge_labels)
+
+plt.title("Graphe des vitesses")
+plt.show()
+
+
+p1 = nx.shortest_path(G, source=0, target=9, weight="weight")
+consumption = nx.shortest_path_length(G, source=0, target=9, weight="weight")
+print("The shortest path from 0 to 9: " , p1)
+print("The lowest consumption is", round(consumption,2)) #arrondi au dixième
